4dce1e035c71dd2c95ed914fc87d3102632945a2
Belhia Fouad <fouad51100@gmail.com>
2023-12-23 18:33:08 +0100

    test

--- deploy/api/controller.php
+++ deploy/api/controller.php
@@ -16,36 +16,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 	    $response->getBody()->write(json_encode ($array));
 	    return $response;
 	}
-	
-	function  getSearchCalatogue (Request $request, Response $response, $args) {
-	    $filtre = $args['filtre'];
-	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
-	   
-	    if ($filtre) {
-	      $data = json_decode($flux, true); 
-	    	
-		$res = array_filter($data, function($obj) use ($filtre)
-		{ 
-		    return strpos($obj["titre"], $filtre) !== false;
-		});
-		$response->getBody()->write(json_encode(array_values($res)));
-	    } else {
-		 $response->getBody()->write($flux);
-	    }
-
-	    return addHeaders ($response);
-	}
 
 	// API Nécessitant un Jwt valide
-	function getCatalogue (Request $request, Response $response, $args) {
-	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
-	    $data = json_decode($flux, true); 
-	    
-	    $response->getBody()->write(json_encode($data));
-	    
-	    return addHeaders ($response);
+	// API pour obtenir le catalogue
+	function getCatalogue(Request $request, Response $response, $args) {
+		global $entityManager;
+		$catalogueRepository = $entityManager->getRepository('Catalogue');
+		$catalogueItems = $catalogueRepository->findAll();
+
+		$catalogueArray = [];
+		foreach ($catalogueItems as $item) {
+			$catalogueArray[] = [
+				'id' => $item->getId(),
+				'name' => $item->getName(),
+				'description' => $item->getDescription(),
+				'price' => $item->getPrice(),
+			];
+		}
+
+		$response->getBody()->write(json_encode($catalogueArray));
+		return addHeaders($response);
 	}
 
+	
 	function optionsUtilisateur (Request $request, Response $response, $args) {
 	    
 	    // Evite que le front demande une confirmation à chaque modification
@@ -56,54 +49,25 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 	// API Nécessitant un Jwt valide
 	function getUtilisateur (Request $request, Response $response, $args) {
-	    global $entityManager;
 	    
 	    $payload = getJWTToken($request);
 	    $login  = $payload->userid;
 	    
-	    $utilisateurRepository = $entityManager->getRepository('Utilisateurs');
-	    $utilisateur = $utilisateurRepository->findOneBy(array('login' => $login));
-	    if ($utilisateur) {
-		$data = array('nom' => $utilisateur->getNom(), 'prenom' => $utilisateur->getPrenom());
-		$response = addHeaders ($response);
-		$response = createJwT ($response);
-		$response->getBody()->write(json_encode($data));
-	    } else {
-		$response = $response->withStatus(404);
-	    }
-
+		$flux = '{"nom":"martin","prenom":"jean"}';
+	    
+	    $response->getBody()->write($flux);
+	    
 	    return addHeaders ($response);
 	}
 
 	// APi d'authentification générant un JWT
 	function postLogin (Request $request, Response $response, $args) {   
-	    global $entityManager;
-	    $err=false;
-	    $body = $request->getParsedBody();
-	    $login = $body ['login'] ?? "";
-	    $pass = $body ['password'] ?? "";
-
-	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
-		$err = true;
-	    }
-	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$pass))  {
-		$err=true;
-	    }
-	    if (!$err) {
-		$utilisateurRepository = $entityManager->getRepository('Utilisateurs');
-		$utilisateur = $utilisateurRepository->findOneBy(array('login' => $login, 'password' => $pass));
-		if ($utilisateur and $login == $utilisateur->getLogin() and $pass == $utilisateur->getPassword()) {
-		    $response = addHeaders ($response);
-		    $response = createJwT ($response);
-		    $data = array('nom' => $utilisateur->getNom(), 'prenom' => $utilisateur->getPrenom());
-		    $response->getBody()->write(json_encode($data));
-		} else {          
-		    $response = $response->withStatus(403);
-		}
-	    } else {
-		$response = $response->withStatus(500);
-	    }
-
+	    
+		$flux = '{"nom":"martin","prenom":"jean"}';
+	    
+	    $response = createJwT ($response);
+	    $response->getBody()->write($flux );
+	    
 	    return addHeaders ($response);
 	}
 
--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,12 +6,12 @@
 	$isDevMode = true;
 	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 	$conn = array(
-	'host' => 'dpg-clvf4ida73kc73bp3kfg-a.oregon-postgres.render.com',
+	'host' => 'dpg-cm23hh7qd2ns73d8dua0-a.oregon-postgres.render.com',
 
 	'driver' => 'pdo_pgsql',
-	'user' => 'cnam_db_akkt_user',
-	'password' => 'I8TW2jVgiSyQDguxH8De6kWjgJ6VjoR4',
-	'dbname' => 'cnam_db_akkt',
+	'user' => 'cnam_db_mn9y_user',
+	'password' => 'VHwiSVl5rD53vVefGgirRHoPxVWmN7X5',
+	'dbname' => 'cnam_db_mn9y',
 	'port' => '5432'
 	);
 
